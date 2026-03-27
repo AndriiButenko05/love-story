a8a96b4f16a3f019e49b313e75347b4ef2a74396
Andrii Butenko <andrejbutik@gmail.com>
2026-03-27 11:19:36 +0100

    Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Моя Королева 👑",
+  title: "Наша история любви",
   description: "Наша история любви",
 };
 
